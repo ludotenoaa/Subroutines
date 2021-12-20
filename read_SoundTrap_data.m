05c1d0fff27a92fd1f451e90875eb4c87c2ca398
ludotenoaa <ludovic.tenorio@noaa.gov>
2021-12-20 07:40:58 -0500

    Create read_SoundTrap_data.m

---
+++ read_SoundTrap_data.m
@@ -0,0 +1,39 @@
+function [x,Fs,t,tstart_file,tend_file]=read_SoundTrap_data(fullname,tstart,tend)
+% inputs:
+% fullname: file name and location
+% tstart/tend: start/end times to load in datenum
+% outputs:
+% x: data
+% Fs: sample frequency
+% t: time vector (s)
+% tstart_file/tend_file: start/end of file in datenum
+
+% extract file name
+tmp=strsplit(fullname,'\');
+fname=tmp{end};
+
+% extract file start time
+tmp=strsplit(fname,'.');
+tstart_file=datenum(tmp{2},'yymmddHHMMSS');
+
+% extract Fs (Hz)
+I=audioinfo(fullname);
+Fs=I.SampleRate;
+
+% extract file end time
+tend_file=tstart_file+I.Duration/86400;
+
+% compute first/last samples based on start/end times
+N=round(Fs*(([tstart tend]-tstart_file)*86400));
+N(1)=N(1)+1;
+
+% load data
+[x,Fs]=audioread(fullname,N);
+
+% time vector (s)
+t=[0:1:length(x)-1]'/Fs;
+
+
+
+
+
