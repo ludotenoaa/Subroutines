a882ef93778697460178c37d27fad52f54158712
ludotenoaa <ludovic.tenorio@noaa.gov>
2022-03-05 07:47:23 -0500

    remove unit 5450 4 hour adjustment
    remove unit 5450 4 hour adjustment

--- find_ST_file.m
+++ find_ST_file.m
@@ -3,10 +3,6 @@ function fname=find_ST_file(t,recorder,fldr,dur)
 % fldr is the full path to the soundtrap folders
 % dur is the SoundTrap file duration in days
 
-if recorder==5450
-    t=t-datenum(0,0,0,4,0,0);
-end
-
 fldr=[fldr,'\',num2str(recorder),'\'];
 
 D=dir(fldr);
